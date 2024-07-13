b7632c7bc4ec9577afed9b49760e699bfa30133f
Mohamed Ali <mo7amedaliebaid@gmail.com>
2024-07-13 23:23:25 +0300

    t

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  basePath: '/web_portfolio_next',
-  assetPrefix: '/web_portfolio_next',
-  output: 'export',
-};
-
-export default nextConfig;
